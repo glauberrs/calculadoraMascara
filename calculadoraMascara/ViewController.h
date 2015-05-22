fe83282486bd41ae37aa12802cddf544c9d55839
Glauber Rodrigues da Silva <glauberrs@gmail.com>
2015-05-22 12:05:17 -0300

    Commit Inicial

--- calculadoraMascara/ViewController.h
+++ calculadoraMascara/ViewController.h
@@ -8,8 +8,18 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UITextFieldDelegate>
 
+@property (weak, nonatomic) IBOutlet UITextField *valor1;
+@property (weak, nonatomic) IBOutlet UITextField *valor2;
+@property (weak, nonatomic) IBOutlet UILabel *resultado;
+- (IBAction)soma:(id)sender;
+- (IBAction)subtracao:(id)sender;
+- (IBAction)divisao:(id)sender;
+- (IBAction)multiplicacao:(id)sender;
+@property (weak, nonatomic) IBOutlet UITextField *semMascara;
+@property (weak, nonatomic) IBOutlet UILabel *comMascara;
+- (IBAction)criarMascara:(id)sender;
 
 @end
 
--- calculadoraMascara/ViewController.m
+++ calculadoraMascara/ViewController.m
@@ -16,12 +16,60 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    _semMascara.delegate = self;
     // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)soma:(id)sender {
+    float meuResultado = _valor1.text.floatValue + _valor2.text.floatValue;
+    _resultado.text = [NSString stringWithFormat:@"%1.2f",meuResultado];
+}
+
+- (IBAction)subtracao:(id)sender {
+    float meuResultado = _valor1.text.floatValue - _valor2.text.floatValue;
+    _resultado.text = [NSString stringWithFormat:@"%1.2f",meuResultado];
+}
+
+- (IBAction)divisao:(id)sender {
+    float meuResultado = _valor1.text.floatValue / _valor2.text.floatValue;
+    _resultado.text = [NSString stringWithFormat:@"%1.2f",meuResultado];
+}
+
+- (IBAction)multiplicacao:(id)sender {
+    float meuResultado = _valor1.text.floatValue * _valor2.text.floatValue;
+    _resultado.text = [NSString stringWithFormat:@"%1.2f",meuResultado];
+}
+- (IBAction)criarMascara:(id)sender {
+    NSString *source = _semMascara.text;
+    NSRange rangeDDD = NSMakeRange(0, 2);
+    NSRange rangePrimeiros4 = NSMakeRange(2, 4);
+    NSRange rangeUltimos4 = NSMakeRange(6, 4);
+    NSString *ddd = [source substringWithRange:rangeDDD];
+    NSString *primeiros4 = [source substringWithRange:rangePrimeiros4];
+    NSString *ultimos4 = [source substringWithRange:rangeUltimos4];
+    NSString *resultadoFinal = [[[
+                       [[@"(" stringByAppendingString:ddd] stringByAppendingString: @") " ]
+                       stringByAppendingString: primeiros4]
+                       stringByAppendingString: @"-"]
+                       stringByAppendingString: ultimos4];
+    _comMascara.text = resultadoFinal;
+    
+    
+}
+
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField{
+    if(textField == _semMascara){
+        [_semMascara resignFirstResponder];
+        [self criarMascara:textField];
+        return YES;
+    }
+    return NO;
 }
 
 @end
